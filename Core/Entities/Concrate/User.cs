f0f4f9f85fbd54a53dc9e0112c4488d28008f35b
Erhan Rohat Turuncu <rohatturuncu@gmail.com>
2021-06-19 03:52:48 +0300

    14. ders 1. kısım

---
+++ Core/Entities/Concrate/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Entities.Concrate
+{
+    //Peki user, operationclaims, useroperationclams neden Core katmanında?
+    //Çünkü JWT Core katmanında. Jwt sınıfları parametre olarak user sınıfı falan kullanıyor.
+    //referans aldıgı için bu sefer core katmanı entities katmanına referans olur
+
+    public class User:IEntity
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public byte[] PasswordHash { get; set; }//veritabanınde binary dediğimiz için byte array seçtik
+        public byte[] PasswordSalt { get; set; }
+        public bool Status { get; set; }
+    }
+}
