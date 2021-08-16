c9fd9791ed30b0bf5e9821507c0a3bf15ad63375
Erhan Rohat Turuncu <rohatturuncu@gmail.com>
2021-08-16 17:55:00 +0300

    20. ders commit
    Custom Error Middleware için fluent validation refactoru

---
+++ Core/Extensions/ErrorDetails.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidation.Results;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+    //Sistemsel hatalar icin
+    public class ErrorDetails
+    {
+        public string Message { get; set; }
+        public int StatusCode { get; set; }
+        
+
+        //bu classı jsona cevirip, öyle response veriyor
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+
+    //validation hatalari icin
+    public class ValidationErrorDetails : ErrorDetails
+    {
+        //fluent validasyon hatalari IEnumerable listesinden geliyor
+        public IEnumerable<ValidationFailure> Errors { get; set; }  
+    }
+
+}
---
+++ Core/Extensions/ExceptionMiddlewareExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Builder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Extensions
+{
+    public static class ExceptionMiddlewareExtensions
+    {
+        //kendi yazdigimiz middleware
+        public static void ConfigureCustomExceptionMiddleware(this IApplicationBuilder app) //this IApplicationBuilder, IApplicationBuilder'i extend ediyor
+        {
+            app.UseMiddleware<ExceptionMiddleware>();//bize middlewareyi genisletmek icin hazir usemiddleware metodu verilmis
+        }
+    }
+}
